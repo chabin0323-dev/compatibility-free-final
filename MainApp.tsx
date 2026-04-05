80f192a082ced9671fa3d78662d7474d10157e54
chabin0323-dev <chabin0323@gmail.com>
2026-04-05 09:42:30 +0900

    Update MainApp.tsx

--- MainApp.tsx
+++ MainApp.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const MainApp: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  return <App />;
 };
 
 export default MainApp;
