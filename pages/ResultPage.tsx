175907c1d5c13d4fabaeabc03350ac86c9746092
chabin0323-dev <chabin0323@gmail.com>
2026-04-23 21:13:21 +0900

    Update ResultPage.tsx

--- pages/ResultPage.tsx
+++ pages/ResultPage.tsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { buildFortuneBundle, type ActiveTabKey } from '../services/fortuneEngine';
 
-const PAID_URL = 'http://pro.k-shiratori.com/';
+const PAID_URL = 'https://aisou-fortune-host.vercel.app/';
 
 type RevealStep =
   | 'intro'
