877cb4b2c6791b642467c8d8591640da34b0b4fb
chabin0323-dev <chabin0323@gmail.com>
2026-06-03 09:47:53 +0900

    Update ResultPage.tsx

--- pages/ResultPage.tsx
+++ pages/ResultPage.tsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { buildFortuneBundle, type ActiveTabKey } from '../services/fortuneEngine';
 
-const PAID_URL = 'https://note.com/like_swan6953';
+const PAID_URL = 'https://note.com/like_swan6953/n/n8381a463b672';
 
 type RevealStep =
   | 'intro'
